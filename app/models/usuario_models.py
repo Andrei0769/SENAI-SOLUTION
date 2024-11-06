2321df1df14aadc2493180d741510b1ed114b067
Andrei Luiz de jesus <159576658+Andrei0769@users.noreply.github.com>
2024-11-06 02:18:05 +0000

    ainda não consegui fazer o pytest

--- app/models/usuario_models.py
+++ app/models/usuario_models.py
@@ -1,20 +1,18 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, UniqueConstraint
 from sqlalchemy.orm import declarative_base
 from config.database import db
 
 Base = declarative_base()
 
-
 class Usuario(Base):
-    # Definindo características da tabela no banco de dados.
     __tablename__ = "usuarios"
+    __table_args__ = (UniqueConstraint('email', name='uix_1'),)  # Adiciona a restrição de unicidade para o email
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     nome = Column(String(150))
     email = Column(String(150))
     senha = Column(String(150))
 
-    # Definindo características da classe.
     def __init__(self, nome: str, email: str, senha: str):
         self.nome = nome
         self.email = email
